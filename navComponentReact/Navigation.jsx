296a4c610b1b2232f7044597c489a79824877dc6
Jr000010 <135720801+Jr000010@users.noreply.github.com>
2024-04-08 15:26:32 +0300

    Create Navigation.jsx

---
+++ navComponentReact/Navigation.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    const menuItems = ['Home','About','Services','Contacts','FAQs','Blog'];
+    
+    return (
+        <nav className="navBasic">
+          <ul>
+              {menuItems.map((item, index) => (
+                  <a  key={index} href="#"><li>{item}</li></a>
+              ))}
+          </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
